8bb4d1524d5f3185de0ac2963361e94157d3ed15
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2018-01-05 18:05:11 +0100

    renamed inner types

--- classic/Flags.h
+++ classic/Flags.h
@@ -4,19 +4,19 @@
 
 namespace classic {
 
-#define ENABLE_CLASSIC_FLAGS_OP(T)                                                                                     \
-    constexpr auto operator|(T::EnumType e1, T::EnumType e2) noexcept->T { return T(e1) | e2; }
+#define ENABLE_CLASSIC_FLAGS_OP(Flags)                                                                                 \
+    constexpr auto operator|(Flags::Enum e1, Flags::Enum e2) noexcept->Flags { return Flags(e1) | e2; }
 
 template<class T>
 struct Flags {
     using This = Flags;
     static_assert(std::is_enum_v<T>, "Flags only works for enums!");
 
-    using EnumType = T;
-    using ValueType = std::underlying_type_t<T>;
+    using Enum = T;
+    using Value = std::underlying_type_t<T>;
 
     template<class... Args>
-    constexpr Flags(EnumType v, Args... args) noexcept
+    constexpr Flags(Enum v, Args... args) noexcept
         : Flags(build(v, args...)) {}
 
     constexpr Flags() noexcept = default;
@@ -26,8 +26,8 @@ struct Flags {
     constexpr auto operator=(const This &) noexcept -> This & = default;
     constexpr auto operator=(This &&) noexcept -> This & = default;
 
-    constexpr auto operator=(T e) noexcept -> This & {
-        v = static_cast<ValueType>(e);
+    constexpr auto operator=(Enum e) noexcept -> This & {
+        v = static_cast<Value>(e);
         return *this;
     }
 
@@ -36,24 +36,24 @@ struct Flags {
     constexpr bool operator==(This f) const noexcept { return v == f.v; }
     constexpr bool operator!=(This f) const noexcept { return v != f.v; }
 
-    constexpr bool operator[](T t) const noexcept { return (v & t) != 0; }
+    constexpr bool operator[](Enum t) const noexcept { return (v & t) != 0; }
 
     constexpr bool all(This f) const noexcept { return (v & f.v) == f.v; }
     template<class... Args>
-    constexpr bool all(T b, Args... args) const noexcept {
+    constexpr bool all(Enum b, Args... args) const noexcept {
         return all(build(b, args...));
     }
 
     constexpr bool any(This f) const noexcept { return (v & f.v) != 0; }
     template<class... Args>
-    constexpr bool any(T b, Args... args) const noexcept {
+    constexpr bool any(Enum b, Args... args) const noexcept {
         return any(build(b, args...));
     }
 
     constexpr bool none() const noexcept { return v == 0; }
     constexpr bool none(This f) const noexcept { return (v & f.v) == 0; }
     template<class... Args>
-    constexpr bool none(T b, Args... args) const noexcept {
+    constexpr bool none(Enum b, Args... args) const noexcept {
         return none(build(b, args...));
     }
 
@@ -61,25 +61,25 @@ struct Flags {
 
     constexpr auto set(This b) const noexcept -> This { return This{v | b.v}; }
     template<class... Args>
-    constexpr auto set(T b, Args... args) const noexcept -> This {
+    constexpr auto set(Enum b, Args... args) const noexcept -> This {
         return set(build(b, args...));
     }
 
     constexpr auto reset(This b) const noexcept -> This { return This{v & ~b.v}; }
     template<class... Args>
-    constexpr auto reset(T b, Args... args) const noexcept -> This {
+    constexpr auto reset(Enum b, Args... args) const noexcept -> This {
         return reset(build(b, args...));
     }
 
     constexpr auto flip(This b) const noexcept -> This { return This{v ^ b.v}; }
     template<class... Args>
-    constexpr auto flip(T b, Args... args) const noexcept -> This {
+    constexpr auto flip(Enum b, Args... args) const noexcept -> This {
         return flip(build(b, args...));
     }
 
     constexpr auto mask(This b) const noexcept -> This { return This{v & b.v}; }
     template<class... Args>
-    constexpr auto mask(T b, Args... args) const noexcept -> This {
+    constexpr auto mask(Enum b, Args... args) const noexcept -> This {
         return mask(build(b, args...));
     }
 
@@ -111,9 +111,9 @@ struct Flags {
 
     template<class F>
     constexpr void each_set(F &&f) const noexcept {
-        auto vt = ValueType{};
+        auto vt = Value{};
         while (true) {
-            const auto t = static_cast<T>(1 << vt);
+            const auto t = static_cast<Enum>(1 << vt);
             const auto ft = Flags{t};
             if (ft.v > v) break;
             if (any(ft)) f(t);
@@ -122,16 +122,16 @@ struct Flags {
     }
 
 private:
-    constexpr Flags(ValueType v) noexcept
+    constexpr Flags(Value v) noexcept
         : v(v) {}
 
     template<class... Args>
     static constexpr auto build(Args... args) noexcept -> Flags {
-        return Flags{(... | static_cast<ValueType>(args))};
+        return Flags{(... | static_cast<Value>(args))};
     }
 
 private:
-    ValueType v{};
+    Value v{};
 };
 
 template<class Out, class T>
@@ -140,7 +140,7 @@ auto operator<<(Out &out, Flags<T> f)
 {
     using flags_type = Flags<T>;
     if (f == flags_type{}) return out << "<None>";
-    f.each_set([&, first = true ](T t) mutable {
+    f.each_set([&, first = true](T t) mutable {
         if (!first)
             out << " | ";
         else
